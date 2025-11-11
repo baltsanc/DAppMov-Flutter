4cd4af05668441d4f11d8ec285546ed0083ea283
baltsanc <baltsanc@hotmail.com>
2025-11-10 22:10:55 -0600

    3.6. Peticiones HTTP

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'Flutter http-API',
-      home: WeatherPage(city: 'Guadalajara'),
+      home: WeatherPage(city: 'Monterrey'),
     );
   }
 }
